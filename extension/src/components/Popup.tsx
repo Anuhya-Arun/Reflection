fd0e3f5f4696356529c9df95018df6fb77ec59f9
Anuhya Arun <anuhya.arun2024@gmail.com>
2026-07-30 18:15:03 +0530

    Add initial Chrome extension manifest and basic UI

--- extension/src/components/Popup.tsx
+++ extension/src/components/Popup.tsx
@@ -1,9 +1,9 @@
-import '../App.css'
+import './Popup.css'
 
 function Popup() {
 
   return (
-    <>
+    <div className="popup">
       <h1>Reflection</h1>
       <p>Your reviewer perspective starts here.</p>
     </>
