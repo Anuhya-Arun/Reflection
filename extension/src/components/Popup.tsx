3823e80eb07b7a986d79330e788a81510a1f4306
Anuhya Arun <anuhya.arun2024@gmail.com>
2026-07-30 17:32:56 +0530

    Create initial React application structure

---
+++ extension/src/components/Popup.tsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+function Popup() {
+
+  return (
+    <>
+      <h1>Reflection</h1>
+      <p>Your reviewer perspective starts here.</p>
+    </>
+  )
+}
+
+export default Popup
